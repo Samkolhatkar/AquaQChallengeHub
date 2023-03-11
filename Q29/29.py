b9b6dd7650a9f344cf3844e9f92bcabbb037162d
SamKolhatkar <kolhatkarsampada@gmail.com>
2023-03-11 19:05:06 +0100

    Solution of Q29

---
+++ Q29/29.py
@@ -0,0 +1,30 @@
+# Python program to use
+
+def goodnumbercount(a):
+    count = 0 
+    for i in range(1,a+1):
+        lista = numbertolist(i)
+        num_count = 0
+        for a in range(0,len(lista)-1):
+            if lista[a] < lista[a+1]:
+                num_count = num_count+1
+
+        # print(num_count)
+        if num_count == len(lista)-1:
+            count = count + 1
+
+    print(count)        
+    return count
+            
+
+    
+
+def numbertolist(a):
+    digitlist = [int(x) for a,x in enumerate(str(a))]
+    return digitlist
+
+
+# main for function call.
+if __name__ == "__main__":
+
+    goodnumbercount(100) #520185742)
